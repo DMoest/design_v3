eb46bd375243c4e0f80a80ae55b8c75dcaba1fca
Daniel Andersson <d.andersson86@icloud.com>
2022-12-05 20:07:23 +0100

    Fix config issue for session variables. Add beginning of a new dark-theme.

--- portfolio/config/config.php
+++ portfolio/config/config.php
@@ -5,3 +5,28 @@ if (session_status() == PHP_SESSION_NONE) {
     session_name($name);
     session_start();
 }
+
+if (isset($_GET["action"])) {
+    // Update session link
+    if ($_GET["action"] == "theme") {
+        $previousValue = isset($_SESSION["theme"]) ? $_SESSION["theme"] : null;
+
+        if ($previousValue == "dark") {
+            unset($_SESSION["theme"]);
+        } else {
+            $_SESSION["theme"] = "dark";
+        }
+
+        $url = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"];
+        $url = preg_replace("/index.php\//", "", $url);
+        header("Location: $url");
+    }
+
+    // Destroy session link
+    if ($_GET["action"] == "session_destroy") {
+        session_destroy();
+        $url = $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"];
+        $url = preg_replace("/index.php\//", "", $url);
+        header("Location: $url");
+    }
+}
--- portfolio/content/_meta.md
+++ portfolio/content/_meta.md
@@ -5,5 +5,10 @@ Social:
 - title: Länk till mitt github repo för kursen.
   url: https://github.com/DMoest/design_v3
   icon: fab fa-github
-
+- title: Dark Mode
+  url: ?action=theme
+  icon: fas fa-moon
+- title: Destroy Session
+  url: ?action=session_destroy
+  icon: fas fa-trash-alt
 ---
